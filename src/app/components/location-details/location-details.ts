1c9a3a965ee5b5070a8408470a7274c12503a9f7
Arunvasu <arun.v@cjlab.com>
2026-05-12 16:33:09 +0530

    assets has been updating in location schema

--- src/app/components/location-details/location-details.ts
+++ src/app/components/location-details/location-details.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CdkDragDrop, DragDropModule, copyArrayItem } from '@angular/cdk/drag-drop';
+import { forkJoin } from 'rxjs';
 
 import { LocationService } from '../../services/location-service';
+import { AssetService } from '../../services/asset';
 
 @Component({
   selector: 'app-location-details',
@@ -14,44 +15,95 @@ import { LocationService } from '../../services/location-service';
 })
 export class LocationDetails implements OnInit {
   locations: any[] = [];
+  assets: any[] = [];
 
   constructor(
     private location: LocationService,
-    // private cdr: ChangeDetectorRef,
+    private assetService: AssetService,
+    private cdr: ChangeDetectorRef,
   ) {}
 
- ngOnInit(): void {
-  const saved = localStorage.getItem('locations');
+  ngOnInit(): void {
 
-  if (saved) {
-    this.locations = JSON.parse(saved);
-  } else {
-    this.location.getLocation().subscribe((res: any) => {
-      this.locations = res.map((loc: any) => ({
-        ...loc,
-        assignedAssets: [],
-      }));
+    forkJoin({
+      locations: this.location.getLocation(),
+      assets: this.assetService.getAssetsList(),
+    }).subscribe((res: any) => {
+
+      this.assets = res.assets;
+
+      // 🔥 LOAD FROM LOCALSTORAGE FIRST (ADDED)
+      const saved = localStorage.getItem('locations');
+      const localData = saved ? JSON.parse(saved) : null;
+
+      this.locations = res.locations.map((loc: any) => {
+
+        let assignedAssets: any[] = [];
+
+        // 🔥 PRIORITY: LOCALSTORAGE DATA (ADDED)
+        const storedLoc = localData?.find((l: any) => l.locationId === loc.locationId);
+
+        if (storedLoc?.assignedAssets) {
+          assignedAssets = storedLoc.assignedAssets;
+        }
+        else if (loc.mapAsset) {
+
+          const ids = loc.mapAsset.split(',');
+
+          ids.forEach((id: string) => {
+            const matchedAsset = this.assets.find((asset: any) => asset.id === id);
+
+            if (matchedAsset) {
+              assignedAssets.push(matchedAsset);
+            }
+          });
+        }
+
+        return {
+          ...loc,
+          assignedAssets,
+        };
+      });
+
+      this.cdr.detectChanges();
     });
   }
-}
 
-  // DRAG DROP
-drop(event: CdkDragDrop<any[]>, location: any) {
-  if (event.previousContainer !== event.container) {
-    const asset = event.previousContainer.data[event.previousIndex];
+  drop(
+    event: CdkDragDrop<any[]>,
+    location: any,
+  ) {
+    if (event.previousContainer !== event.container) {
 
-    // prevent undefined array crash
-    if (!location.assignedAssets) {
-      location.assignedAssets = [];
+      // UI UPDATE
+      copyArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+
+      // DRAGGED ASSET
+      const draggedAsset = event.previousContainer.data[event.previousIndex];
+
+      // API PAYLOAD
+      const payload = {
+        locationId: location.locationId,
+        assetId: draggedAsset.id,
+      };
+
+      // SAVE API
+      this.location.mapAssetToLocation(payload).subscribe({
+        next: (res: any) => {
+
+          console.log('MAPPED SUCCESS', res);
+          localStorage.setItem('locations', JSON.stringify(this.locations));
+        },
+
+        error: (err) => {
+          console.log('API ERROR', err);
+        },
+      });
     }
-
-    // add asset
-    location.assignedAssets.push(asset);
-    localStorage.setItem('locations', JSON.stringify(this.locations));
   }
-}
-
-//   saveToStorage() {
-//   localStorage.setItem('locations', JSON.stringify(this.locations));
-// }
 }
